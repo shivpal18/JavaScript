5532c105831bfcade72b64393f39e305a15d88d8
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-03-24 23:20:55 +0530

    update

---
+++ 6_datatypes.js
@@ -0,0 +1,21 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigInt = 123456534735242n
+
+// Reference (Non-Primitive) 
+
+// Array, Objects, Functions
