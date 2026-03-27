aaa00c00eed19d19278fbf222d7feb9f762861d6
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-03-27 23:40:45 +0530

    update

---
+++ 9_numberAndMath.js
@@ -0,0 +1,22 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 1345.78452714
+console.log(otherNumber.toPrecision(5));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//Math
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
