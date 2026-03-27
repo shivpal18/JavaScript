ee004ee266a1796c30f2a295af13d1f31b3004e9
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-03-28 00:49:00 +0530

    update

--- 9_numberAndMath.js
+++ 9_numberAndMath.js
@@ -19,4 +19,14 @@ console.log(Math.abs(-4));
 console.log(Math.round(4.6));
 console.log(Math.ceil(4.2));
 console.log(Math.floor(4.9));
+console.log(Math.min(4, 2, 7, 9, 4));
+console.log(Math.max(4, 2, 7, 9, 4));
 
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
