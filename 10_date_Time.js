c58815795face8bc12e45ca16725064db0f583fe
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-03-29 23:36:33 +0530

    update

---
+++ 10_date_Time.js
@@ -0,0 +1,9 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
