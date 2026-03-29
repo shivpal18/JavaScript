ebabd71f1e2b56ded5ea0bd368e25d44a1854268
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-03-29 23:56:29 +0530

    update

--- 10_date_Time.js
+++ 10_date_Time.js
@@ -7,3 +7,15 @@ console.log(myDate.toDateString());
 console.log(myDate.toLocaleString());
 
 console.log(typeof myDate);
+
+// let newCreatedDate = new Date(2026, 2, 29)
+// let newCreatedDate = new Date(2026, 2, 29, 5, 3)
+// let newCreatedDate = new Date("03-29-2026")
+let newCreatedDate = new Date("03-29-2026")
+console.log(newCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(newCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
