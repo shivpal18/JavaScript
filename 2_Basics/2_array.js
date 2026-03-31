f7b37a1ea7276954ed42ee0745dfd33a2a88ff1d
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-03-31 23:51:45 +0530

    update

---
+++ 2_Basics/2_array.js
@@ -0,0 +1,11 @@
+const marvel_heros = ["Thor", "Ironman", "Hulk"]
+const dc_heros = ["Spiderman", "Flash", "Batman"]
+
+//marvel_heros.push(dc_heros)
+
+const all_heros = marvel_heros.concat(dc_heros)
+
+console.log(marvel_heros);
+//console.log(marvel_heros[3]);
+
+console.log(all_heros);
