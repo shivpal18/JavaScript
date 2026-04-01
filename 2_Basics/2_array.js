f0b320c0e5b98e7c9bd3ad0b13b8c72d09fe29c2
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-04-01 22:54:45 +0530

    update

--- 2_Basics/2_array.js
+++ 2_Basics/2_array.js
@@ -9,3 +9,17 @@ console.log(marvel_heros);
 //console.log(marvel_heros[3]);
 
 console.log(all_heros);
+
+const array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_array = array.flat(Infinity)
+console.log(real_array);
+
+console.log(Array.isArray("Shivpal"));
+console.log(Array.from("Shivpal"));
+console.log(Array.from({name: "Shivpal"}));  //Return empty array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
---
+++ 2_Basics/3_objects.js
@@ -0,0 +1,17 @@
+// singleton
+//object.create
+
+// objects literals
+const JsUser = {
+    name: "Shivpal",
+    "Full Name": "Shivpal Chauasiya",
+    age: 20,
+    location: "UP",
+    email: "Shivpal@google.com",
+    isLoedIn: false,
+    lasLoedIn: ["Monday", "Friday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["Full Name"]);
