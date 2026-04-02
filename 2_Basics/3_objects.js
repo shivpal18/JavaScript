93c11e6a7033fe231ed5002c4d64e58b724cbc5c
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-04-02 23:35:40 +0530

    update

--- 2_Basics/3_objects.js
+++ 2_Basics/3_objects.js
@@ -2,9 +2,14 @@
 //object.create
 
 // objects literals
+
+const mySym = Symbol("key1")
+
+
 const JsUser = {
     name: "Shivpal",
     "Full Name": "Shivpal Chauasiya",
+    [mySym]: "mykey1",
     age: 20,
     location: "UP",
     email: "Shivpal@google.com",
@@ -12,6 +17,22 @@ const JsUser = {
     lasLoedIn: ["Monday", "Friday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["Full Name"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["Full Name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "shivpal@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "shivpal@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function() {
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function() {
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
