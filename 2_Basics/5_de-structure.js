5083fbf1a32626a897d8cbed73858b7fbaa468a2
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-04-03 23:43:09 +0530

    update

---
+++ 2_Basics/5_de-structure.js
@@ -0,0 +1,13 @@
+const course = {
+    coursename: "JS",
+    price: "999",
+    courseInstructor: "Shivpal"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+
