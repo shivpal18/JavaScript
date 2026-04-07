4ff495c6264f99e45500ecfb4bbac2993dcc9b89
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-04-08 01:08:29 +0530

    update

---
+++ 5_Loops/2_while.js
@@ -0,0 +1,12 @@
+let i = 0
+while (i <= 10) {
+    console.log(`${i}`);
+    i = i + 2
+}
+
+let myArray = ["batman", "superman", "spidrman", "ironman"]
+let arr = 0
+while (arr < myArray.length){
+    console.log(`${myArray[arr]}`);
+    arr = arr + 1
+}
---
+++ 5_Loops/3_doWhile.js
@@ -0,0 +1,5 @@
+let score = 1
+do {
+    console.log(`${score}`);
+    score++
+} while (score <= 10)
