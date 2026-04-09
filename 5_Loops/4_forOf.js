3abdca6b006c92255140db9c041fcb610f2fcda2
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-04-09 22:01:04 +0530

    update

---
+++ 5_Loops/4_forOf.js
@@ -0,0 +1,23 @@
+// For Of loop
+
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings= "Hello World!"
+for(const greet of greetings){
+    // console.log(`Each char is ${greet}`);   
+}
+
+//  Map
+
+const map = new Map()
+map.set('IN', 'India')
+map.set('USA', 'United States Of America')
+map.set('FR', 'France')
+// console.log(map);
+
+for(const [key, value] of map){
+    console.log(key, ':-', value);
+}
