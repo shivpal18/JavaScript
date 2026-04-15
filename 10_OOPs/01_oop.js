4fc74a1e4e10a3ddf2a60599eb6153a0870d9e1c
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-04-16 00:02:27 +0530

    update

---
+++ 10_OOPs/01_oop.js
@@ -0,0 +1,31 @@
+const user = {
+    username: "Shivpal",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log("Got user detail from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);   
+    }
+}
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, signedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.signedIn = signedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+    return this
+}
+const userOne = new User("Shivpal", 12, true)
+const userTwo = new User("JS", 14, false)
+console.log(userOne.constructor);
+console.log(userTwo);
---
+++ 10_OOPs/notes.md
@@ -0,0 +1,23 @@
+# javascript and classes
+- JavaScript objects are collections of key-value pairs used to store and organize related data in a structured way.
+
+## OOP
+- OOP is a programming paradigm based on objects that contain data and methods to model real-world entities.
+
+## Object
+- collection of properties and methods
+- toLowerCase
+
+## why use OOP
+- OOP is used to make code more organized, reusable, and easier to maintain by using objects and classes.
+
+## parts of OOP
+- The main parts of OOP are Inheritance, Polymorphism, Encapsulation, and Abstraction.
+- These are also called 4 pillers in OOP
+- (object literal)
+
+## Some keyword
+- Constructor function
+- Prototypes
+- Classes
+- Instance
